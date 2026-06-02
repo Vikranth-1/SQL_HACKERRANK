a840938e9de035e4fd9e4a37809d8312348d3904
Vikranth V <vvikranth08@gmail.com>
2026-06-02 06:00:52 +0530

    Create Symmetric Pairs.sql

---
+++ Symmetric Pairs.sql
@@ -0,0 +1,1 @@
+SELECT f1.X, f1.Y FROM Functions f1 WHERE EXISTS (SELECT 1 FROM Functions f2 WHERE f1.X = f2.Y AND f1.Y = f2.X) AND f1.X < f1.Y UNION SELECT f1.X, f1.Y FROM Functions f1 WHERE f1.X = f1.Y GROUP BY f1.X, f1.Y HAVING COUNT(*) > 1 ORDER BY X;
