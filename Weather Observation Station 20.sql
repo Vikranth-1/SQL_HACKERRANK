08a13089e38c971d74b1a269ca3fc63212ecd918
Vikranth V <vvikranth08@gmail.com>
2026-06-03 07:57:45 +0530

    Create Weather Observation Station 20.sql

---
+++ Weather Observation Station 20.sql
@@ -0,0 +1,1 @@
+SET @rowindex := -1; SELECT ROUND(AVG(LAT_N), 4) FROM (SELECT @rowindex := @rowindex + 1 AS rowindex, LAT_N FROM STATION ORDER BY LAT_N) AS subquery WHERE subquery.rowindex IN (FLOOR(@rowindex / 2), CEIL(@rowindex / 2));
